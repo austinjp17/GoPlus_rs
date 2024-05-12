598c49613620019eb07d5608e5d9051c2efe21b0
Austin Palmer <44661648+austinjp17@users.noreply.github.com>
2024-05-12 14:34:42 -0500

    all endpoints added, 2 more to connect

--- src/api_structs.rs
+++ src/api_structs.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use serde::{Deserialize, Serialize};
 
 
@@ -145,4 +146,225 @@ pub struct AccountResult {
     phishing_activities: String,
     sanctioned: String,
     stealing_attack: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ASresponse {
+    code: u32,
+    message: String,
+    result: ASresult,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ASresult {
+    contract_name: Option<String>,
+    creator_address: Option<String>,
+    deployed_time: Option<u64>,  // Assuming it's a timestamp in seconds
+    doubt_list: u32,
+    is_contract: u32,
+    is_open_source: Option<u32>,
+    is_proxy: Option<u32>,
+    malicious_behavior: Vec<String>,
+    tag: Option<String>,
+    trust_list: u32,
+    contract_scan: ContractScan,
+    risky_approval: RiskyApproval
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct RiskyApproval {
+    value: u8,
+    risk: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ContractScan {
+    owner: Option<Owner>,
+    privilege_withdraw: Option<String>,
+    withdraw_missing: Option<String>,
+    blacklist: Option<String>,
+    selfdestruct: Option<String>,
+    approval_abuse: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Owner {
+    owner_name: Option<String>,
+    owner_address: Option<String>,
+    owner_type: Option<String>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NftRiskResponse {
+    code: u32,
+    message: String,
+    result: NftData,
+}
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NftData {
+    average_price_24h: f64,
+    create_block_number: Option<u64>,
+    creator_address: Option<String>,
+    discord_url: Option<String>,
+    github_url: Option<String>,
+    highest_price: f64,
+    lowest_price_24h: f64,
+    malicious_nft_contract: u32,
+    medium_url: Option<String>,
+    metadata_frozen: Option<u8>,
+    nft_address: String,
+    nft_description: Option<String>,
+    nft_erc: String,
+    nft_items: u32,
+    nft_name: String,
+    nft_open_source: u8,
+    nft_owner_number: u32,
+    nft_proxy: Option<u8>,
+    nft_symbol: Option<String>,
+    nft_verified: u32,
+    oversupply_minting: Option<u8>,
+    privileged_burn: PrivilegedAction,
+    privileged_minting: PrivilegedAction,
+    red_check_mark: Option<()>,
+    restricted_approval: Option<u8>,
+    sales_24h: u32,
+    same_nfts: Vec<SameNft>,
+    self_destruct: PrivilegedAction,
+    telegram_url: Option<String>,
+    token_id: Option<String>,
+    token_owner: Option<String>,
+    total_volume: f64,
+    traded_volume_24h: f64,
+    transfer_without_approval: PrivilegedAction,
+    trust_list: u32,
+    twitter_url: Option<String>,
+    website_url: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PrivilegedAction {
+    owner_address: Option<String>,
+    owner_type: Option<String>,
+    value: Option<u8>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct SameNft {
+    create_block_number: u64,
+    nft_address: String,
+    nft_name: String,
+    nft_owner_number: u32,
+    nft_symbol: Option<String>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PhishingSiteResponse {
+    code: u32,
+    message: String,
+    result: PhishingSiteResult,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct PhishingSiteResult {
+    pub phishing_site: i32,
+    pub website_contract_security: Vec<WebsiteContractSecurity>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct WebsiteContractSecurity {
+    pub address_risk: Vec<String>,
+    pub contract: String,
+    pub is_contract: i32,
+    pub is_open_source: i32,
+    pub nft_risk: NftRisk,
+    pub standard: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NftRisk {
+    pub nft_open_source: i32,
+    pub nft_proxy: i32,
+    pub oversupply_minting: i32,
+    pub privileged_burn: PrivilegedAction,
+    pub privileged_minting: PrivilegedAction,
+    pub restricted_approval: i32,
+    pub self_destruct: PrivilegedAction,
+    pub transfer_without_approval: PrivilegedAction,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct RugPullRiskResponse {
+    code: u32,
+    message: String,
+    result: RugPullResult,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct RugPullResult {
+    pub approval_abuse: i32,
+    pub blacklist: i32,
+    pub contract_name: String,
+    pub is_open_source: i32,
+    pub is_proxy: i32,
+    pub owner: Option<Owner>,
+    pub privilege_withdraw: i32,
+    pub selfdestruct: i32,
+    pub withdraw_missing: i32,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct AccessCodeResponse {
+    pub code: u32,
+    pub message: String,
+    pub result: Option<AccessCodeResult>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct AccessCodeResult {
+    pub access_token: String,
+    pub expires_in: u64
+}
+#[derive(Deserialize, Debug, Clone)]
+pub struct AbiDecodeResponse {
+    pub code: u32,
+    pub message: String,
+    pub result: AbiDecodeResult,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct AbiDecodeResult {
+    pub contract_description: Option<String>,
+    pub contract_name: String,
+    pub data: Option<serde_json::Value>,
+    pub malicious_contract: Option<u8>,
+    pub method: String,
+    pub params: Vec<Param>,
+    pub risk: Option<String>,
+    pub risky_signature: Option<i32>,
+    pub signature_detail: Option<String>,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Param {
+    pub address_info: Option<AddressInfo>,
+    pub input: String,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub type_field: Option<String>,
+    pub tuple: Option<serde_json::Value>,
+    #[serde(rename = "struct")]
+    pub struct_field: Option<serde_json::Value>
+    
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct AddressInfo {
+    pub contract_name: String,
+    pub is_contract: i32,
+    pub malicious_address: i32,
+    pub name: String,
+    pub standard: String,
+    pub symbol: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-use reqwest::{self, Client, Response};
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use reqwest::Client;
+
 
 mod api_structs;
 use api_structs::*;
+use serde_json::{json, Value};
 
 const BASE_URL: &str = "https://api.gopluslabs.io/api/v1";
+
+
 pub struct Session {
     inner: Client,
+    #[allow(dead_code)]
     access_token: Option<String>,
 }
 
@@ -27,38 +30,85 @@ impl Session {
         Self {
             inner: Client::new(),
             access_token: None,
+            
         }
     }
 
-    pub async fn supported_chains(&self, access_token: Option<&str>) -> Result<SupportedChainsResponse, anyhow::Error> {
-        let url = format!("{BASE_URL}/supported_chains");
-        let token = match access_token {
-            Some(t) => t,
-            None => "None",
+    /// Obtains an access token using SHA-1 signature method.
+    ///
+    /// # Sign Method
+    /// Concatenate `app_key`, `time`, and `app_secret` in turn, and apply SHA-1 hashing.
+    ///
+    /// # Example
+    /// ```
+    /// let app_key = "mBOMg20QW11BbtyH4Zh0";
+    /// let time = 1647847498;
+    /// let app_secret = "V6aRfxlPJwN3ViJSIFSCdxPvneajuJsh";
+    /// let sign = "sha1(mBOMg20QW11BbtyH4Zh01647847498V6aRfxlPJwN3ViJSIFSCdxPvneajuJsh)"; // This results in `7293d385b9225b3c3f232b76ba97255d0e21063e`
+    /// ```
+    ///
+    /// # Parameters
+    /// - `app_key`: Application key provided by the service.
+    /// - `signature`: Computed SHA-1 hash as a string.
+    /// - `time`: Current time as a UNIX timestamp.
+    ///
+    /// # Example Usage
+    /// ```
+    /// let mut instance = Session::new(None);
+    /// instance.get_access_token("mBOMg20QW11BbtyH4Zh0", "7293d385b9225b3c3f232b76ba97255d0e21063e", 1647847498);
+    /// ```
+    pub async fn get_access_token(&mut self, app_key: &str, signature: &str, time: u64) -> Result<(), anyhow::Error> {
+        let url = format!("{}/token", BASE_URL);
+        // How to do body params?
+
+        let params = json!({
+            "app_key": app_key,
+            "sign": signature,
+            "time": time,
+        });
+
+        let access_code_res = self.inner.get(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .json(&params)
+            .send()
+            .await?
+            .json::<AccessCodeResponse>()
+            .await?;
+
+        // access_code_res.result.unwrap().expires_in;
+        if access_code_res.code == 1 {
+            tracing::info!("New access token expires in {} minutes", (access_code_res.result.as_ref().unwrap().expires_in)/60);
+
+            self.access_token = Some(access_code_res.result.unwrap().access_token);
+        } else {
+            tracing::error!("Error getting access token\nCode: {}", access_code_res.code)
+            // ERROR HANDLING
         };
+        
+        Ok(())
+
+    }
+    
+    pub async fn supported_chains(&self) -> Result<SupportedChainsResponse, anyhow::Error> {
+        let url = format!("{BASE_URL}/supported_chains");
 
         Ok(self
             .inner
             .get(url)
-            .header("access_token", token)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
             .send()
             .await?
             .json::<SupportedChainsResponse>()
             .await?)
     }
 
-    pub async fn token_security(&self, chain_id: &str, addr: &str, access_token: Option<&str>) -> Result<TokenResponse, anyhow::Error> {
+    pub async fn token_risk(&self, chain_id: &str, addr: &str) -> Result<TokenResponse, anyhow::Error> {
         let url = format!(
             "{}/token_security/{}", BASE_URL, chain_id
         );
 
-        let token = match access_token {
-            Some(t) => t,
-            None => "None",
-        };
-
         Ok(self.inner.get(url)
-            .header("access_token", token)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
             .query(&[("contract_addresses", addr)])
             .send()
             .await?
@@ -66,26 +116,135 @@ impl Session {
             .await?)
     }
 
-    pub async fn address_security(&self, addr: &str, chain_id: Option<&str>, access_token: Option<&str>) -> Result<AccountResponse, anyhow::Error> {
+    pub async fn address_risk(&self, addr: &str, chain_id: Option<&str>) -> Result<AccountResponse, anyhow::Error> {
         let url = format!("{}/address_security/{}", BASE_URL, addr);
 
-        let access_token = match access_token {
-            Some(t) => t,
-            None => "None",
-        };
-
-        let chain_id = match chain_id {
-            Some(id) => id,
-            None => "None",
-        };
-
         Ok(self.inner.get(url)
-            .header("access_token", access_token)
-            .query(&[("chain_id", chain_id)])
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .query(&[("chain_id", chain_id.unwrap_or("None"))])
             .send()
             .await?
             .json::<AccountResponse>()
             .await?)
     }
 
+    pub async fn approval_security_v1(&self, chain_id: &str, contract_addr: &str) -> Result<ASresponse, anyhow::Error> {
+        let url = format!("{}/approval_security/{}", BASE_URL, chain_id);
+
+        Ok(self.inner.get(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .query(&[("contract_addresses", contract_addr)])
+            .send()
+            .await?
+            .json::<ASresponse>()
+            .await?)
+    }
+
+    // TODO
+    pub async fn approval_security_v2(&self) {
+        todo!();
+    }
+
+    pub async fn abi_decode(&self, 
+        chain_id: &str, 
+        data: &str,
+        contract_addr: Option<&str>,
+        signer: Option<&str>,
+        txn_type: Option<&str>
+    ) -> Result<AbiDecodeResponse, anyhow::Error> {
+        
+        let url = format!("{}/abi/input_decode", BASE_URL);
+
+        let params = json!({
+            "chain_id": chain_id,
+            "data": data,
+            "contract_address": contract_addr,
+            "signer": signer,
+            "transaction_type": txn_type
+        });
+
+        Ok(self.inner.post(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .json(&params)
+            .send()
+            .await?
+            .json::<AbiDecodeResponse>()
+            .await?)
+    }
+
+
+    pub async fn nft_risk(&self, chain_id: &str, contract_addr: &str, token_id: Option<&str>) -> Result<NftRiskResponse, anyhow::Error> {
+        let url = format!("{}/nft_security/{}",BASE_URL, chain_id);
+
+        Ok(self.inner.get(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .query(&[("contract_addresses", contract_addr), ("token_id", token_id.unwrap_or("None"))])
+            .send()
+            .await?
+            .json::<NftRiskResponse>()
+            .await?)
+    }
+
+    // TODO: No successfully found url
+    pub async fn dapp_risk_by_url(&self, dapp_url: &str) -> Result<Value, anyhow::Error> {
+        todo!("Fails on all tried urls idk");
+        let url = format!("{}/dapp_security", BASE_URL);
+        
+        Ok(self.inner.get(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .query(&[("url", dapp_url)])
+            .send()
+            .await?
+            .json::<Value>()
+            .await?)
+    }
+
+    pub async fn phishing_site_risk(&self, site_url: &str) -> Result<PhishingSiteResponse, anyhow::Error> {
+        let url = format!("{}/phishing_site", BASE_URL);
+
+        Ok(self.inner.get(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .query(&[("url", site_url)])
+            .send()
+            .await?
+            .json::<PhishingSiteResponse>()
+            .await?)
+    }
+    
+    pub async fn rug_pull_risk(&self, chain_id: &str, contract_addr: &str) -> Result<RugPullRiskResponse, anyhow::Error> {
+        let url = format!("{}/rugpull_detecting/{}", BASE_URL, chain_id);
+
+        Ok(self.inner.get(url)
+            .header("access_token", self.access_token.clone().unwrap_or("None".to_string()))
+            .query(&[("contract_addresses", contract_addr)])
+            .send()
+            .await?
+            .json::<RugPullRiskResponse>()
+            .await?)
+    }
+}
+
+
+
+pub fn interpret_status_code(code: u32) -> &'static str {
+    match code {
+        1 => "Complete data prepared",
+        2 => "Partial data obtained. The complete data can be requested again in about 15 seconds.",
+        2004 => "Contract address format error!",
+        2018 => "ChainID not supported",
+        2020 => "Non-contract address",
+        2021 => "No info for this contract",
+        2022 => "Non-supported chainId",
+        2026 => "dApp not found",
+        2027 => "ABI not found",
+        2028 => "The ABI not support parsing",
+        4010 => "App_key not exist",
+        4011 => "Signature expiration (the same request parameters cannot be requested more than once)",
+        4012 => "Wrong Signature",
+        4023 => "Access token not found",
+        4029 => "Request limit reached",
+        5000 => "System error",
+        5006 => "Param error!",
+        _ => "Unknown status code",
+    }
 }
